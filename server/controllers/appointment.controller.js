99302e93e274e39681349afb1f3cdc563e44559e
Alok <you@example.com>
2017-12-01 17:32:04 +0530

    cancel

--- server/controllers/appointment.controller.js
+++ server/controllers/appointment.controller.js
@@ -5,16 +5,25 @@ var appointment = require('services/appointment.service');
 
 // routes
 router.post('/book', bookAppointment);
-router.delete('/cancel?bid', bookAppointment);
+router.post('/cancel', cancelAppointment);
 module.exports = router;
 
 function bookAppointment(req,res) {
     appointment.bookAppointment(req.body)
     .then(function (obj) {
-        res.status(400).send(obj);
+        res.status(200).send(obj);
     })
     .catch(function(err) {
          res.status(400).send(err);
     })
 
+}
+function cancelAppointment(req, res) {
+    appointment.cancelAppointment(req.body)
+    .then(function() {
+        res.status(200).send("ok");
+    })
+    .catch(function(err) {
+        res.status(400).send(err);
+    })
 }
