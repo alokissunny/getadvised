88d2b1d6fb5ee593fd5017420bfda2bf29e31ba9
alokissunny <bit.srivastava@gmail.com>
2018-05-06 21:34:14 +0530

    chat chnages

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(expressJwt({
         }
         return null;
     }
-}).unless({ path: ['/users/authenticate', '/users/register','/advisors/register' ,/\/chat/i ,/\/socket.io/i, /\/advisors\/get/i  , '/upload',/\/advisors\/current/i ] }));
+}).unless({ path: ['/users/authenticate', '/listFriends', '/users/register', '/advisors/register', /\/chat/i, /\/socket.io/i, /\/advisors\/get/i, '/upload', /\/advisors\/current/i] }));
 
 // routes
 app.use('/users', require('./controllers/users.controller'));
@@ -41,14 +41,87 @@ var server = app.listen(port, function () {
 });
 var io = require('socket.io')(server);
 // socket io
+// io.on('connection', function (socket) {
+//     console.log('User connected');
+//     socket.on('disconnect', function() {
+//       console.log('User disconnected');
+//     });
+//     socket.on('save-message', function (data) {
+//       console.log(data);
+//       io.emit('new-message', { message: data });
+//     });
+//   });
+var usersCollection = [];
+
+// Express routes
+// app.set("view engine", "vash");
+
+// app.get("*",function(req, res){
+//   res.render("index");
+// });
+
+app.post("/listFriends", function (req, res) {
+    var clonedArray = usersCollection.slice();
+
+    // Getting the userId from the request body as this is just a demo 
+    // Ideally in a production application you would change this to a session value or something else
+    var i = usersCollection.findIndex(x => x.id == req.body.userId);
+
+    clonedArray.splice(i, 1);
+
+    res.json(clonedArray);
+});
+
+// Socket.io operations
 io.on('connection', function (socket) {
-    console.log('User connected');
-    socket.on('disconnect', function() {
-      console.log('User disconnected');
+    console.log('A user has connected to the server.');
+
+    socket.on('join', function (userInfo) {
+        // Same contract as ng-chat.User
+        username = userInfo.username;
+        var index = usersCollection.findIndex(x => {
+            return x.username == username;
+        });
+        if (index != -1) {
+            return;
+        }
+        if (userInfo.isExpert) {
+            usersCollection.push({
+                id: socket.id, // Assigning the socket ID as the user ID in this example
+                displayName: username,
+                status: 0, // ng-chat UserStatus.Online,
+                avatar: null
+            });
+        }
+
+
+        socket.broadcast.emit("friendsListChanged", usersCollection);
+
+        console.log(username + " has joined the expert room.");
+
+        // This is the user's unique ID to be used on ng-chat as the connected user.
+        socket.emit("generatedUserId", socket.id);
+
+        // On disconnect remove this socket client from the users collection
+        socket.on('disconnect', function () {
+            console.log('User disconnected!');
+
+            var i = usersCollection.findIndex(x => x.id == socket.id);
+            usersCollection.splice(i, 1);
+
+            socket.broadcast.emit("friendsListChanged", usersCollection);
+        });
     });
-    socket.on('save-message', function (data) {
-      console.log(data);
-      io.emit('new-message', { message: data });
+
+    socket.on("sendMessage", function (message) {
+        console.log("Message received:");
+        console.log(message);
+
+        io.to(message.toId).emit("messageReceived", {
+            user: usersCollection.find(x => x.id == message.fromId),
+            message: message
+        });
+
+        console.log("Message dispatched.");
     });
-  });
-  
+});
